a963882f42a4443bb995311e99ee7f4a89bf042e
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-06-12 01:43:47 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def display_board(board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "])
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+  return board
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    return true
+  else 
+    return false
+  end
+end
+
+def turn(board)
+  input = 0
+  index = 
+  until input_to_index(input) == valid_move?(board, index)
+  puts "invalid"
+  input += 1
+  if valid_move?(board, index) == true
+    return board
+    end    
+  end
+end
